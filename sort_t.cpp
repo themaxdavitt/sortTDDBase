fe59eca6e7f4b3fecd3e4c8c028d7db07e118a26
Max Davitt <med112@zips.uakron.edu>
2020-10-15 15:10:10 -0400

    Sort for in-order 3 element vector

--- sort_t.cpp
+++ sort_t.cpp
@@ -63,5 +63,22 @@ int main() {
         assert(v[1] == 2);
     }
 
+    {
+        std::vector<int> v;
+        v.push_back(1);
+        v.push_back(2);
+        v.push_back(3);
+        assert(!v.empty());
+        assert(v.size() == 3);
+
+        sort(v);
+
+        assert(!v.empty());
+        assert(v.size() == 3);
+        assert(v[0] == 1);
+        assert(v[1] == 2);
+        assert(v[2] == 3);
+    }
+
     return 0;
 }
